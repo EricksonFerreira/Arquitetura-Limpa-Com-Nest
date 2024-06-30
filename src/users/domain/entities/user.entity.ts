fd73c37a1092d768009238f0278035988952d45e
Erickson Ferreira <erickson.rinho@hotmail.com>
2024-06-30 16:54:47 -0300

    Adicionando metodos update no UserEntity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -15,8 +15,18 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
+  update(value: string) {
+    this.name = value;
+  }
+  updatePassword(value: string) {
+    this.password = value;
+  }
+
   get name(): string {
     return this.props.name;
+  }
+  private set name(value: string) {
+    this.props.name = value;
   }
 
   get email(): string {
@@ -25,6 +35,10 @@ export class UserEntity extends Entity<UserProps> {
 
   get password(): string {
     return this.props.password;
+  }
+
+  private set password(value: string) {
+    this.props.password = value;
   }
 
   get createdAt(): Date {
