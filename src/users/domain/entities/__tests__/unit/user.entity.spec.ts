cb2ea484b52a42559ff277c74fda38fd8ddfb1a0
Erickson Ferreira <erickson.rinho@hotmail.com>
2024-06-30 16:15:57 -0300

    Adicionar o helper UserDataBuild

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,17 +1,13 @@
 import { UserProps } from './../../user.entity';
-import { faker } from '@faker-js/faker';
 import { UserEntity } from '../../user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-date-builder';
 
 describe('User entity unit tests', () => {
   let props: UserProps;
   let sut: UserEntity;
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+    props = UserDataBuilder({});
 
     sut = new UserEntity(props);
   });
