4ef697306ca9798124f11aeea558e1f74857253b
Erickson Ferreira <erickson.rinho@hotmail.com>
2024-06-30 17:52:26 -0300

    Teste unitario na ClassValidatorFields

---
+++ src/users/domain/entities/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,45 @@
+import { ClassValidatorFields } from '../../class-validator-fields';
+import * as libClassValidator from 'class-validator';
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field: string;
+}> {}
+
+describe('ClassValidatorFields unit tests', () => {
+  it('Should initialize errors and validatedDate variables with null', () => {
+    const sut = new StubClassValidatorFields();
+    expect(sut.errors).toBeNull();
+    expect(sut.validatedData).toBeNull();
+  });
+
+  it('Should validate with errors', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync');
+    spyValidateSync.mockReturnValue([
+      {
+        property: 'field',
+        constraints: {
+          isRequired: 'test error',
+        },
+      },
+    ]);
+    const sut = new StubClassValidatorFields();
+
+    expect(sut.validate(null)).toBeFalsy();
+    expect(spyValidateSync).toHaveBeenCalled();
+    expect(sut.validatedData).toBeNull();
+    expect(sut.errors).toStrictEqual({
+      field: ['test error'],
+    });
+  });
+
+  it('Should validate without errors', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync');
+    spyValidateSync.mockReturnValue([]);
+    const sut = new StubClassValidatorFields();
+
+    expect(sut.validate({ field: 'test' })).toBeTruthy();
+    expect(spyValidateSync).toHaveBeenCalled();
+    expect(sut.errors).toBeNull();
+    expect(sut.validatedData).toStrictEqual({ field: 'test' });
+  });
+});
