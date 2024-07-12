85c42a6b095d6b715d69bfeaed245917fbf82411
Erickson Ferreira <erickson.rinho@hotmail.com>
2024-07-12 18:55:41 -0300

    Adicionando a classe SearchParams

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -1,10 +1,75 @@
 import { Entity } from '../entities/entity';
 import { RepositoryInterface } from './repository-contracts';
 
+export type SortDirection = 'asc' | 'desc';
+export type SearchProps<Filter = string> = {
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export class SearchParams {
+  protected _page: number;
+  protected _perPage: number;
+  protected _sort: string | null;
+  protected _sortDir: SortDirection | null;
+  protected _filter: string | null;
+
+  constructor(props: SearchProps) {
+    this._page = props.page || 1;
+    this._perPage = props.perPage || 15;
+    this._sort = props.sort || null;
+    this._sortDir = props.sortDir || null;
+    this._filter = props.filter || null;
+  }
+
+  get page(): number {
+    return this._page;
+  }
+
+  set page(value: number) {
+    this._page = value;
+  }
+
+  get perPage(): number {
+    return this._perPage;
+  }
+
+  private set perPage(value: number) {
+    this._perPage = value;
+  }
+
+  get sort(): string | null {
+    return this._sort;
+  }
+
+  private set sort(value: string | null) {
+    this._sort = value;
+  }
+
+  get sortDir(): SortDirection | null {
+    return this._sortDir;
+  }
+
+  private set sortDir(value: SortDirection | null) {
+    this._sortDir = value;
+  }
+
+  get filter(): string | null {
+    return this._filter;
+  }
+
+  private set filter(value: string | null) {
+    this._filter = value;
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,
   SearchOutput,
 > extends RepositoryInterface<E> {
-  search(search: SearchInput): Promise<SearchOutput>;
+  search(search: SearchParams): Promise<SearchOutput>;
 }
