0dff05ecb24d87f21b5414d2568a6febd03d77d9
Erickson Ferreira <erickson.rinho@hotmail.com>
2024-07-13 16:11:14 -0300

    Adicionando o metodo applySort no InMemorySearchableRepository

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -10,6 +10,8 @@ export abstract class InMemorySeachableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
+  sortableFields: string[] = [];
+
   async search(props: SearchParams): Promise<SearchResults<E>> {
     const itemsFiltered = await this.applyFilter(this.items, props.filter);
     const itemsSorted = await this.applySort(
@@ -38,15 +40,28 @@ export abstract class InMemorySeachableRepository<E extends Entity>
   protected abstract applyFilter(
     items: E[],
     filter: string | null,
-  ): Promise<E[]> {}
+  ): Promise<E[]>;
 
-  protected abstract applySort(
+  protected async applySort(
     items: E[],
     sort: string | null,
     sortDir: string | null,
-  ): Promise<E[]> {}
+  ): Promise<E[]> {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items;
+    }
+    return [...items].sort((a, b) => {
+      if (a.props[sort] < b.props[sort]) {
+        return sortDir === 'asc' ? -1 : 1;
+      }
+      if (a.props[sort] > b.props[sort]) {
+        return sortDir === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+  }
 
-  protected applyPagination(
+  protected async applyPagination(
     items: E[],
     page: SearchParams['page'],
     perPage: SearchParams['perPage'],
--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -137,5 +137,6 @@ export interface SearchableRepositoryInterface<
   SearchInput = SearchParams,
   SearchOutput = SearchResults<E, Filter>,
 > extends RepositoryInterface<E> {
+  sortableFields: string[];
   search(search: SearchInput): Promise<SearchOutput>;
 }
